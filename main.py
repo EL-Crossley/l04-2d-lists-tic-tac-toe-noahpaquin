1fcccdbc758b26d2f6bad7cb05f97af529e47ddf
noahpaquin <noahpaquin7@gmail.com>
2024-11-04 13:25:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# Put your function here
+def ticTacToe(board):
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2]:  
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i]:  
+            return board[0][i]
 
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
 
-# testing
+    return "No one"
+
 board = [['X','O','O'],['O','X','O'],['X','O','X']]
-print(ticTacToe(board))
+print("" + ticTacToe(board) + " won!")
